c036631b094f6398e365a042f05d22ba12d8b2ea
José Quezada Pinto <j.mquezadapinto@gmail.com>
2020-02-18 14:37:54 -0300

    Creacion script para abrir paneles

--- Assets/Script/CambiarPanel.cs
+++ Assets/Script/CambiarPanel.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class CambiarPanel : MonoBehaviour
 {
+    public GameObject PrimerPanel;
+    public GameObject SegundoPanel;
+    public GameObject TercerPanel;
+    public GameObject CuartoPanel; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrimerPanel.SetActive(false);
+        SegundoPanel.SetActive(false);
+        TercerPanel.SetActive(false);
+        CuartoPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void AbrirPanel()
     {
-        
+        if(PrimerPanel != null)
+        {
+            PrimerPanel.SetActive(true);
+        }
+
+        if (SegundoPanel != null)
+        {
+            SegundoPanel.SetActive(true);
+        }
+
+        if (TercerPanel != null)
+        {
+            TercerPanel.SetActive(true);
+        }
+
+        if (CuartoPanel != null)
+        {
+            CuartoPanel.SetActive(true);
+        }
     }
+    
+
+    
+    
 }
